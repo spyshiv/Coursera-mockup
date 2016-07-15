48ff7f80454ab23fa90fd9d9443968b06da97625
spyshiv <spyshiv@gmail.com>
2016-07-15 21:31:14 +0530

    Add slider and some tweaks

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@ jQuery(function($){
 
 // 1. SEARCH FORM
 
-   jQuery('#co-search-icon').on('click', function(event) {
+    jQuery('#co-search-icon').on('click', function(event) {
         event.preventDefault();
         $('#co-search').addClass('co-search-open');
         $('#co-search form input[type="search"]').focus();
@@ -10,6 +10,16 @@ jQuery(function($){
     
     jQuery('.co-search-close').on('click', function(event) {
       $("#co-search").removeClass('co-search-open');
+    });
+
+// 2. TOP SLIDER
+    jQuery('#co-slider').slick({
+      dots: false,
+      infinite: true,
+      arrows: true,
+      speed: 500,     
+      autoplay: true,      
+      cssEase: 'linear'
     });
 
 });
