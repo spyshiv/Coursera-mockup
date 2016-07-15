9d4e269516f5cb44e0874b7eb9807567f6add73a
spyshiv <spyshiv@gmail.com>
2016-07-15 23:39:54 +0530

    Add about us, latest course slider

--- static/js/main.js
+++ static/js/main.js
@@ -22,4 +22,60 @@ jQuery(function($){
       cssEase: 'linear'
     });
 
+// 3. ABOUT US VIDEO
+    // WHEN CLICK PLAY BUTTON 
+    jQuery('#co-abtus-video').on('click', function(event) {
+      event.preventDefault();
+      $('body').append("<div id='about-video-popup'><span id='co-video-close' class='fa fa-close'></span><iframe id='cotube-video' name='cotube-video' frameborder='0' allowfullscreen></iframe></div>");        
+      $("#cotube-video").attr("src", $(this).attr("href"));
+    });         
+    // WHEN CLICK CLOSE BUTTON
+    $(document).on('click','#co-video-close', function(event) {     
+      $(this).parent("div").fadeOut(1000);
+    });
+    // WHEN CLICK OVERLAY BACKGROUND
+    $(document).on('click','#about-video-popup', function(event) {
+      $(this).remove();
+    });
+
+// 4. Latest Courses
+    jQuery('#co-latest-course-slide').slick({
+      dots: false,
+      arrows: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 2500,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: false,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
 });
+
