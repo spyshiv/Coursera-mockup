3a7af178b9d19949f514ebde472ab03f19a32fa1
spyshiv <spyshiv@gmail.com>
2016-07-16 03:45:30 +0530

    Add search page

--- coursera/urls.py
+++ coursera/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^search', views.search, name='search'),
+
 ]
 
--- coursera/views.py
+++ coursera/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.conf import settings
-
+import requests
+import json
 # Create your views here.
 def index(request):
 	return render(request, 'index.html', {"static_url": settings.STATIC_URL})
+
+def search(request):
+	if request.method == 'POST':
+		search_query = request.POST.get('search-query')
+		url = "https://api.coursera.org/api/courses.v1?q=search&query="
+		fields = "name,partnerIds,partnerLogo" 
+		includes = "instructorIds,partnerIds"
+		response = requests.get(url + search_query + "&fields=" + fields + "&includes=" + includes).json()
+		total = response['paging']['total']
+		elements = response['elements']
+	return render(request, 'search-result.html', {'total':total, 'elements':elements})
