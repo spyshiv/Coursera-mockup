e472b54f847c96132dea1d41e07a57553a604486
spyshiv <spyshiv@gmail.com>
2016-07-18 21:02:46 +0530

    Add minor tweak

--- static/js/main.js
+++ static/js/main.js
@@ -78,8 +78,47 @@ jQuery(function($){
       ]
     });
 
-// 5. Latest Courses
+// 5. Courses
     jQuery('#co-latest-course-slide').slick({
+      dots: false,
+      arrows: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 2500,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: false,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+
+    jQuery('#co-popular-slide').slick({
       dots: false,
       arrows: true,
       infinite: false,
